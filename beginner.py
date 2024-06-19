c32ae1c55b93b71792012a652380c71fe1e48b7e
Ogojoshua905 <joshuaexcellency1@gmail.com>
2024-06-19 16:08:09 +0100

    feat: created file

---
+++ beginner.py
@@ -0,0 +1,36 @@
+# def joshua():
+#     print("step one")
+#     print("step two")
+# print("step three")
+# print("standalone print")
+# joshua("")
+
+# num = 5
+# num2 = 4.0
+# isStudent=True
+# print(num + num)
+
+# print(7 + num)
+# name= "henry"
+
+# print(name)
+# print("welcome " + name)
+
+# first_class = "Welcome to Python Class June"
+# print(first_class)
+
+num = 9 #int
+price=20.00 # float
+is_student=False #boolean
+gender="male" #string
+numbers=[30, 12, 1, 18] #list
+grades =("A", "B", "C", "D") #tuple
+scores = {"a":20, "b":12} #dict
+age_set = {20, 19, 18} #set
+
+
+
+print(type(price))
+print(type("this"))
+first_name=input("enter your Name:")
+print(first_name)
